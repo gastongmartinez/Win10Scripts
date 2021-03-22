6b71e1105f686738bc8c1928f7eb279668721fc6
Gastón Martínez <gastongmartinez@gmail.com>
2021-03-22 00:45:41 -0300

    Renombrado de archivo

--- 2_Security.ps1
+++ 2_Security.ps1
@@ -1,5 +1,5 @@
 # Comentar para omitir
-$tweaks = @(
+$privacy = @(
     "DisableTelemetry",
     "DisableActivityHistory",
     "DisableBackgroundApps",
@@ -9,15 +9,8 @@ $tweaks = @(
     "DisableAdvertisingID",
     "DisableCortana",
     "DisableErrorReporting",
-    "DisableDiagTrack",
-    "HideTaskbarSearch",
-    "HideTaskView",
-    "ShowUserFolderOnDesktop",
-    "ShowThisPCOnDesktop",
-    "ShowNetworkOnDesktop",
-    "UnpinStartMenuTiles"
+    "DisableDiagTrack"
 )
-
 
 Function DisableTelemetry {
     Write-Output "Disabling Telemetry..."
@@ -116,59 +109,7 @@ Function DisableDiagTrack {
     Set-Service "DiagTrack" -StartupType Disabled
 }
 
-Function HideTaskbarSearch {
-    Write-Output "Hiding Taskbar Search icon / box..."
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Type DWord -Value 0
-}
-
-Function HideTaskView {
-    Write-Output "Hiding Task View button..."
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Type DWord -Value 0
-}
-
-Function ShowUserFolderOnDesktop {
-    Write-Output "Showing User Folder shortcut on desktop..."
-    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu")) {
-        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu" -Force | Out-Null
-    }
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu" -Name "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" -Type DWord -Value 0
-    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel")) {
-        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Force | Out-Null
-    }
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" -Type DWord -Value 0
-}
-
-Function ShowThisPCOnDesktop {
-    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel")) {
-        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Force | Out-Null
-    }
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Type DWord -Value 0
-}
-
-Function ShowNetworkOnDesktop {
-    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel")) {
-        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Force | Out-Null
-    }
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" -Type DWord -Value 0
-}
-
-Function UnpinStartMenuTiles {
-    Write-Output "Unpinning all Start Menu tiles..."
-    If ([System.Environment]::OSVersion.Version.Build -ge 15063 -And [System.Environment]::OSVersion.Version.Build -le 16299) {
-        Get-ChildItem -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" -Include "*.group" -Recurse | ForEach-Object {
-            $data = (Get-ItemProperty -Path "$($_.PsPath)\Current" -Name "Data").Data -Join ","
-            $data = $data.Substring(0, $data.IndexOf(",0,202,30") + 9) + ",0,202,80,0,0"
-            Set-ItemProperty -Path "$($_.PsPath)\Current" -Name "Data" -Type Binary -Value $data.Split(",")
-        }
-    }
-    ElseIf ([System.Environment]::OSVersion.Version.Build -eq 19042) {
-        $key = Get-ChildItem -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" -Recurse | Where-Object { $_ -like "*start.tilegrid`$windows.data.curatedtilecollection.tilecollection\Current" }
-        $data = (Get-ItemProperty -Path $key.PSPath -Name "Data").Data[0..25] + ([byte[]](202, 50, 0, 226, 44, 1, 1, 0, 0))
-        Set-ItemProperty -Path $key.PSPath -Name "Data" -Type Binary -Value $data
-    }
-}
-
-# Call the desired tweak functions
-$tweaks | ForEach-Object { Invoke-Expression $_ }
+# Call the desired privacy functions
+$privacy | ForEach-Object { Invoke-Expression $_ }
 
 Write-Output "Reinicie el equipo"
