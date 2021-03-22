af3d27c527a305822b90de62700aec6bf1e5d248
Gastón Martínez <gastongmartinez@gmail.com>
2021-03-22 01:25:40 -0300

    Remover Edge

--- 1_Debloat.ps1
+++ 1_Debloat.ps1
@@ -101,7 +101,8 @@ $Bloat = @(
     "WindowsTips",
     "SettingsSuggestedContent",
     "WhatsNewInWindows",
-    "BingSearch"
+    "BingSearch",
+    "UnpinTaskbarEdge"
 )
 Function WindowsWelcomeExperience {
     New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SubscribedContent-310093Enabled -PropertyType DWord -Value 0 -Force
@@ -131,6 +132,41 @@ Function BingSearch {
     New-ItemProperty -Path HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Name DisableSearchBoxSuggestions -PropertyType DWord -Value 1 -Force
 }
 
+Function UnpinTaskbarEdge {
+    $Signature = @{
+        Namespace        = "WinAPI"
+        Name             = "GetStr"
+        Language         = "CSharp"
+        MemberDefinition = @"
+	[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+	public static extern IntPtr GetModuleHandle(string lpModuleName);
+	[DllImport("user32.dll", CharSet = CharSet.Auto)]
+	internal static extern int LoadString(IntPtr hInstance, uint uID, StringBuilder lpBuffer, int nBufferMax);
+	public static string GetString(uint strId)
+	{
+		IntPtr intPtr = GetModuleHandle("shell32.dll");
+		StringBuilder sb = new StringBuilder(255);
+		LoadString(intPtr, strId, sb, sb.Capacity);
+		return sb.ToString();
+	}
+"@
+    }
+    if (-not ("WinAPI.GetStr" -as [type])) {
+        Add-Type @Signature -Using System.Text
+    }
+
+    # Extract the localized "Unpin from taskbar" string from shell32.dll
+    $LocalizedString = [WinAPI.GetStr]::GetString(5387)
+    $Apps = (New-Object -ComObject Shell.Application).NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items()
+    $VerbExists = $Apps | Where-Object -FilterScript { $_.Path -eq "MSEdge" } | ForEach-Object -Process { $_.Verbs() | Where-Object -FilterScript { $_.Name -eq $LocalizedString } }
+    if ($VerbExists) {
+        $VerbExists | ForEach-Object -Process { $_.DoIt() }
+    }
+    else {
+        Remove-Item -Path "$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk" -ErrorAction Ignore
+        $VerbExists | ForEach-Object -Process { $_.DoIt() }
+    }
+}
 
 # Comentar para omitir
 RemoveAppsCapabilities
