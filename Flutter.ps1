d30d8140375a33623704e8c280b4b8a16e3645b5
Gastón Martínez <gastongmartinez@gmail.com>
2021-04-11 03:30:06 -0300

    Cambio de nombre

--- Flutter.ps1
+++
@@ -1,6 +0,0 @@
-New-Item -Path "c:\" -Name "flutter" -ItemType "directory"
-git clone https://github.com/flutter/flutter.git -b stable "c:\flutter"
-
-[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";C:\flutter\bin", [EnvironmentVariableTarget]::User)
-
-flutter doctor
